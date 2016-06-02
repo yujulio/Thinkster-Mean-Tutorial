d9356557f56b8e8435813f4b786454bf8e542a4e
Julio Yu <jyu@smartappsla.com>
2016-06-02 16:52:30 -0400

    Angular Routing

---
+++ app.js
@@ -0,0 +1,54 @@
+var app = angular.module('flapperNews',['ui-router']);
+
+app.factory('posts', [function(){
+    // service body
+    var o = {
+        posts: []
+    };
+
+    return o;
+}]);
+
+app.controller('MainCtrl', [
+
+    '$scope', 'posts', function($scope, posts){
+        $scope.test = "Hello world!";
+        $scope.posts = posts.posts;
+        $scope.addPost = function(){
+                if (!$scope.title || $scope.title === '') {
+                    return;
+                }
+                $scope.posts.push(
+                    {
+                        title:$scope.title,
+                        link:$scope.link,
+                        upvotes: 0
+                    }
+                );
+                $scope.title='';
+                $scope.link='';
+        };
+        $scope.incrementUpvotes = function (post) {
+            post.upvotes +=1;
+        }
+    }
+]);
+
+app.config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    function ($stateProvider,$urlRouterProvider) {
+        $stateProvider
+            .state('home',{
+                url:'/home',
+                templateUrl:'/home.html',
+                controller:'MainCtrl'
+            }).state('posts',{
+                url: '/posts/{id}',
+                templateUrl: '/posts.html',
+                controller: 'PostsCtrl'
+            });
+
+        $urlRouterProvider.otherwise('home');
+    }
+]);
